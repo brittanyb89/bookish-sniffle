bbf463752fb5aa589465d19bc252fe6ac7bc9657
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-12-16 18:58:08 -0600

    Added TODO notes for guidance

---
+++ src/generateHTML.js
@@ -0,0 +1,5 @@
+// TODO: Create a function that returns a Github profile based on the username
+
+// TODO: Create a function that returns an email link based on the email
+
+// TODO: Create a function that generate the HTML based on the user input
