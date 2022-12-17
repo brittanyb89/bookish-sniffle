dd3a0c167ae8643e08d7fbbf46bed7ca77c78078
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-12-16 18:55:55 -0600

    Added guided notes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+// TODO: Include packages needed for this application
+// import inquirer from "inquirer";
+
+// TODO: Create an array of questions for user input
+
+// TODO: Create a function to generate HTML from user input
+
+// TODO: Create a function to initialize app
