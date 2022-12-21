fe601c22b43522a407a8c6dd1d04672c8de13179
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-12-20 23:25:08 -0600

    Created foeach loop and joined function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,65 +1,117 @@
-const inquirer = require('inquirer');
-const { writeFile} = require("fs").promises;
-// import fs from "fs";
+const inquirer = require("inquirer");
+const { writeFile } = require("fs").promises;
+import listMembers from "./index.js";
 
-
-// Import HTML template
-const teamMembers = [];
-const employees = [name, id, email];
-
-const generateHTML = require({ ...employees, engineer, intern, manager }) =>
- `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <title>Bookish-Sniffle</title>
-   <!-- Favicon -->
-  <link rel="icon" href="/src/images/team image.png" type="image/x-icon">
-</head>
-<body>
-  <!-- Main content -->
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1 class="text-center">Team of Software Engineers</h1>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-12">
-        <h2 class="text-center">Team Members</h2>
-      </div>
-    </div>
-    <div class="row">
+function Members(list) {
+  list.forEach((element) => {
+    if (element.getRole() === "Manager") {
+      return `<div class="row">
       <div class="col-12">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Name: ${name} <span id="name"></span></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Role: ${teamMemberType} <span id="role"></span></h6>
-            <p class="card-text">ID: ${id} <span id="id"></span></p>
-            <p class="card-text">Email: ${email} <span id="email"></span></p>
-            <p class="card-text">${teamMemberType.manager, intern, engineer}<span id="default"></span></p>
+            <h5 class="card-title">Name: ${element.name} <span id="name"></span></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Role: ${element.getRole}<span id="role"></span></h6>
+            <p class="card-text">ID: ${element.ID} <span id="id"></span></p>
+            <a href="email.com" class="card-text"> ${element.email} <span id="email"></span></a>
+            <a href class="card-text" src="github.com"> ${element.officeNumber}<span id="default"></span></a>
+          </div>
+        </div>
+      </div>`;
+    }
+
+    if (element.getRole() === "Engineer") {
+      return `<div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Name: ${element.name} <span id="name"></span></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Role: ${element.getRole}<span id="role"></span></h6>
+            <p class="card-text">ID: ${element.ID} <span id="id"></span></p>
+            <a href="email.com" class="card-text"> ${element.email} <span id="email"></span></a>
+            <a href class="card-text" src="github.com"> ${element.github}<span id="default"></span></a>
+          </div>
+        </div>
+      </div>`;
+    }
+
+    if (element.getRole() === "Intern") {
+      return `<div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Name: ${element.name} <span id="name"></span></h5>
+            <h6 class="card-subtitle mb-2 text-muted">Role: ${element.getRole}<span id="role"></span></h6>
+            <p class="card-text">ID: ${element.ID} <span id="id"></span></p>
+            <a href="email.com" class="card-text"> ${element.email} <span id="email"></span></a>
+            <a href class="card-text" src="github.com"> ${element.school}<span id="default"></span></a>
+          </div>
+        </div>
+      </div>`;
+    }
+  });
+}
+
+// TODO: push string into new array
+const generateHTML = (list) => {
+  const html = Members(list);
+  return html;
+};
+
+// TODO: join array into one string
+const generatePage = (html) => {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Bookish-Sniffle</title>
+      <!-- Favicon -->
+    <link rel="icon" href="/src/images/team image.png" type="image/x-icon">
+  </head>
+  <body>
+    <!-- Main content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <h2 class="card-title
+              ">My Team</h2>
+            </div>
           </div>
         </div>
       </div>
+      ${html}
     </div>
-  <!-- Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <script type="module" src="index.js"></script>
-</body>
-</html>`;
+  </body>
+  </html>`;
+};
 
-const init()  => {
-  promptUser()
+// TODO: create html file
+const writeFile = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile("./dist/index.html", data, (err) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: "File created!",
+      });
+    });
+  });
+};
+
+const init = () => {
+  listMembers()
     .then((answers) => writeFile("generatedHTML.html", generateHTML(answers)))
     .then(() => console.log("Successfully wrote to generatedHTML.html"))
     .catch((err) => console.error(err));
 };
 
 init();
-
-
-
